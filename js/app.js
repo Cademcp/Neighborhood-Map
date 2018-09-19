4de6cd030a868d9359f94f53647256922d2a6611
Cade McPartlin <cademcp@gmail.com>
2018-09-19 13:43:37 -0500

    Added all markers on load. Beginning render of marker list

--- js/app.js
+++ js/app.js
@@ -1,4 +1,3 @@
-
 // Points of interest that will be shown to user
 var locations = [
     {title: 'Bradley University', location: {lat: 40.697827, lng: -89.615298}},
@@ -9,46 +8,76 @@ var locations = [
     {title: 'Peoria Civic Center', location: {lat: 40.692055, lng: -89.593948}}
 ];
 
+var listInfo = function (locations) {
+    
+}
+
+// START VIEWMODEL?
+
 var map;
+
+var markers = [];
 
 // Function to initialize the map within the map div
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
+        // initial center is Peoria, Il coordinates
         center: {lat: 40.6936, lng: -89.5890},
         zoom: 14
     });
-    // Create a single latLng literal object.
-    var singleLatLng = {lat: 40.698760, lng: -89.615093};
-    // TODO: Create a single marker appearing on initialize -
-    // Create it with the position of the singleLatLng,
-    // on the map, and give it your own title!
+
+    // Created a single marker to display on the map in the location selected from
+    // locations array
     var marker = new google.maps.Marker({
         position: locations[0]['location'],
         map: map,
         title: locations[0]['title']
     });
-    // TODO: create a single infowindow, with your own content.
-    // It must appear on the marker
+    // Creating an info window to appear when the marker is clicked
     var largeInfowindow = new google.maps.InfoWindow();
+    var bounds = new google.maps.LatLngBounds();
 
-    function populateInfoWindow(marker, infowindow) {
-        // Check to make sure the infowindow is not already opened on this marker.
-        if (infowindow.marker != marker) {
-            infowindow.marker = marker;
-            infowindow.setContent('<div>' + marker.title + '</div>');
-            infowindow.open(map, marker);
-            // Make sure the marker property is cleared if the infowindow is closed.
-            infowindow.addListener('closeclick',function(){
-                infowindow.setMarker = null;
-            });
-        }
+    // A loop to put all of the markers into an array and display them
+    for (var i = 0; i < locations.length; i++) {
+        // Get the position from the location array.
+        var position = locations[i].location;
+        var title = locations[i].title;
+        // Create a marker per location, and put into markers array.
+        var marker = new google.maps.Marker({
+            map: map,
+            position: position,
+            title: title,
+            animation: google.maps.Animation.DROP,
+            id: i
+        });
+        // Add marker to array
+        markers.push(marker);
+        // Put onclick in the loop so that each marker has it applied
+        marker.addListener('click', function () {
+            populateInfoWindow(this, largeInfowindow);
+        });
+        bounds.extend(markers[i].position);
     }
-
-    // TODO: create an EVENT LISTENER so that the infowindow opens when
-    // the marker is clicked!
-    marker.addListener('click', function () {
-        populateInfoWindow(this, largeInfowindow);
-    });
+    // Extend the boundaries of the map for each marker
+    map.fitBounds(bounds);
 }
+
+function populateInfoWindow(marker, infowindow) {
+    // Check to make sure the infowindow is not already opened on this marker.
+    if (infowindow.marker != marker) {
+        infowindow.marker = marker;
+        infowindow.setContent('<div>' + marker.title + '</div>');
+        infowindow.open(map, marker);
+        // Make sure the marker property is cleared if the infowindow is closed.
+        infowindow.addListener('closeclick', function () {
+            infowindow.setMarker = null;
+        });
+    }
+}
+
+// Event listener to show the InfoWindow when the marker is clicked
+marker.addListener('click', function () {
+    populateInfoWindow(this, largeInfowindow);
+});
 
 

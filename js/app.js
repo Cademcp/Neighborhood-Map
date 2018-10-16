241d131fd78803310ce1dcc7fbc0bbee5847c7b3
Cade McPartlin <cademcp@gmail.com>
2018-10-16 11:10:17 -0500

    Adding error handling

--- js/app.js
+++ js/app.js
@@ -59,31 +59,41 @@ let Marker = function (info) {
     $.getJSON(venue_request, function (data) {
         get_venues = data;
 
+        let venues = get_venues['response']['venues'][0];
+        // pull out venue id for next api call
+
+        self.address = venues['location']['formattedAddress'][0];
+        self.cityStateZip = venues['location']['formattedAddress'][1];
+        self.country = venues['location']['formattedAddress'][2];
+
         venue_id = get_venues['response']['venues'][0]['id'];
 
         let picture_request = 'https://api.foursquare.com/v2/venues/' + venue_id + '/photos?client_id=' + clientID + '&client_secret=' + clientSecret + '&v=20181015';
 
         $.getJSON(picture_request, function (data) {
 
-            let venues = get_venues['response']['venues'][0];
-            // pull out venue id for next api call
-
-            self.address = venues['location']['formattedAddress'][0];
-            self.cityStateZip = venues['location']['formattedAddress'][1];
-            self.country = venues['location']['formattedAddress'][2];
-            console.log(data);
             let picture_url_prefix = data['response']['photos']['items'][0]['prefix'];
-            console.log(picture_url_prefix);
+
             let picture_url_suffix = data['response']['photos']['items'][0]['suffix'];
-            console.log(picture_url_suffix);
-            let picture_width = data['response']['photos']['items'][0]['width'];
-            let picture_height = data['response']['photos']['items'][0]['height'];
 
             self.venue_image = picture_url_prefix + '200' + 'x' + '200' + picture_url_suffix;
             console.log('HERE');
             console.log(self.venue_image);
         }).fail(function () {
-            alert("test");
+            // checks to see if this error message already exists, if it does then don't display it.
+            // produces an error message for the user saying that foursquare images will not be loaded.
+            if (!document.getElementById('image-error')) {
+                let tag = document.createElement("p");
+                tag.setAttribute('id', 'image-error');
+                tag.style.color = 'red';
+                let node = document.createTextNode("Foursquare could not load images at this time. (Most likely due to premium daily request limit of 50)");
+                tag.appendChild(node);
+                let header = document.getElementById('header');
+                header.appendChild(tag);
+            } else {
+                return;
+            }
+
         });
 
     }).fail(function () {
@@ -162,7 +172,15 @@ function populateInfoWindow(marker, address, city, country, image, infowindow) {
     // Check to make sure the infowindow is not already opened on this marker.
     if (infowindow.marker != marker) {
         infowindow.marker = marker;
-        infowindow.setContent('<div>' + '<br><strong>' + marker.title + '</strong><br><br>' + address + '<br>' + city + '<br>' + country + '<br>' + '<img src=\"' + image + '\">' + '</div>');
+
+        // checking to see if the image was pulled from foursquare
+        // this is a premium feature of the foursquare API so there is a 50 request limit per day
+        // if the image is not there, then don't show it
+        if (image === undefined) {
+            infowindow.setContent('<div>' + '<br><strong>' + marker.title + '</strong><br><br>' + address + '<br>' + city + '<br>' + country + '</div>');
+        } else {
+            infowindow.setContent('<div>' + '<br><strong>' + marker.title + '</strong><br><br>' + address + '<br>' + city + '<br>' + country + '<br>' + '<img src=\"' + image + '\">' + '</div>');
+        }
         infowindow.open(map, marker);
         // Make sure the marker property is cleared if the infowindow is closed.
         infowindow.addListener('closeclick', function () {
